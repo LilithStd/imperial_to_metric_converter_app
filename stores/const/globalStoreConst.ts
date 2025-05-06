190c65ae0a3b838b23643dcf8cec20f49e42961c
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2025-05-06 20:00:43 +0300

    add global store and helpers consts

---
+++ stores/const/globalStoreConst.ts
@@ -0,0 +1,10 @@
+export enum THEME_APP {
+	DARK = 'dark',
+	LIGHT = 'light',
+}
+
+export enum LANGUAGE_APP {
+	EN = 'en',
+	RU = 'ru',
+	LV = 'lv',
+}
--- stores/global.ts
+++ stores/global.ts
@@ -1,17 +1,22 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {create} from 'zustand';
 import {createJSONStorage, persist} from 'zustand/middleware';
+import {LANGUAGE_APP, THEME_APP} from './const/globalStoreConst';
 
 interface GlobalStoreInterface {
-	currentTheme: string;
+	currentTheme: THEME_APP;
+	currentLanguage: LANGUAGE_APP;
 	setCurrentTheme: () => void;
+	setCurrentLanguage: () => void;
 }
 
 export const useGlobalStore = create<GlobalStoreInterface>()(
 	persist(
 		(set, get) => ({
-			currentTheme: '',
+			currentTheme: THEME_APP.LIGHT,
+			currentLanguage: LANGUAGE_APP.EN,
 			setCurrentTheme: () => {},
+			setCurrentLanguage: () => {},
 		}),
 		{
 			name: 'global-storage',
