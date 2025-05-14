236737c90fdcd9dafd3b2217fca17a77e3d0d63a
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2025-05-14 16:22:16 +0300

    add theme switch (base logic)

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,22 @@
 import LanguageSwitcher from '@/components/languageSwitcher'
 import { APP_PATH_ROUTE } from '@/stores/const/globalConsts'
+import { THEME_APP } from '@/stores/const/globalStoreConst'
+import { useGlobalStore } from '@/stores/globalStore'
 import { mainScreenStyles } from '@/styles/mainScreenStyles'
 import { useRouter } from 'expo-router'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import MoonIcons from '../assets/images/icons/moon.svg'
+import SunIcons from '../assets/images/icons/sun.svg'
 const defaultBackgroundImage = require('../assets/images/backgrounds/bg_00.jpg')
 const greenButton = require('../assets/images/buttons/greenButton(Small).png')
 
+
+
 export default function MainScreen() {
     const router = useRouter()
+    const currentAppTheme = useGlobalStore(state => state.currentTheme)
+    const setCurrentTheme = useGlobalStore(state => state.setCurrentTheme)
 
     const redirectToListValuesScreen = () => {
         router.push({
@@ -22,12 +30,24 @@ export default function MainScreen() {
         })
     }
 
+    const switchCurrentAppTheme = () => {
+        const switchTheme = currentAppTheme === THEME_APP.LIGHT ? THEME_APP.DARK : THEME_APP.LIGHT
+        setCurrentTheme(switchTheme)
+    }
+
     return (
         <SafeAreaView style={mainScreenStyles.mainContainer}>
             <ImageBackground
                 style={mainScreenStyles.mainImageBackground}
                 source={defaultBackgroundImage}
             >
+                <TouchableOpacity
+                    onPress={switchCurrentAppTheme}
+                >
+
+                    {currentAppTheme === THEME_APP.LIGHT ? <SunIcons width={24} height={24} fill="green" /> : <MoonIcons width={24} height={24} fill="green" />}
+
+                </TouchableOpacity>
                 <View style={mainScreenStyles.mainTitleContainer}>
                     <Text style={mainScreenStyles.mainTitle}>Main Screen</Text>
                 </View>
--- stores/globalStore.ts
+++ stores/globalStore.ts
@@ -6,7 +6,7 @@ import {LANGUAGE_APP, THEME_APP} from './const/globalStoreConst';
 interface GlobalStoreInterface {
 	currentTheme: THEME_APP;
 	currentLanguage: LANGUAGE_APP;
-	setCurrentTheme: () => void;
+	setCurrentTheme: (theme: THEME_APP) => void;
 	setCurrentLanguage: (language: LANGUAGE_APP) => void;
 }
 
@@ -15,7 +15,13 @@ export const useGlobalStore = create<GlobalStoreInterface>()(
 		(set, get) => ({
 			currentTheme: THEME_APP.LIGHT,
 			currentLanguage: LANGUAGE_APP.EN,
-			setCurrentTheme: () => {},
+			setCurrentTheme: (theme) => {
+				if (theme === get().currentTheme) {
+					return;
+				} else {
+					set({currentTheme: theme});
+				}
+			},
 			setCurrentLanguage: (language) => {
 				if (language === get().currentLanguage) {
 					return;
