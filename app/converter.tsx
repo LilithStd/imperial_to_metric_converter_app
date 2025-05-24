9a342b4482a157b78f41dc65312a28c0b416001a
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2025-05-24 19:48:51 +0300

    add converter view and basic makrup

--- app/converter.tsx
+++ app/converter.tsx
@@ -4,6 +4,7 @@ import { useValuesStore } from "@/stores/valuesStore";
 import { converterScreenStyles } from "@/styles/converterScreenStyles";
 import { FlatList, ImageBackground, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import SwitchValueArrow from '../assets/images/icons/repeat.svg';
 
 const defaultBackground = require('../assets/images/backgrounds/bg_00.jpg')
 
@@ -11,19 +12,41 @@ export default function Convertor() {
     const valuesListStore = useValuesStore(state => state.getListValues)
     const currentLanguage = useGlobalStore(state => state.currentLanguage)
     const valuesListToView = valuesListStore(VALUES_TYPES.ALL, currentLanguage)
+
     const renderItem = ({ item }: { item: RESULT_VALUES_TYPE }) => (
-        <View>
-            <FlatList
-                data={item.values}
-                nestedScrollEnabled={true}
-                renderItem={({ item }) => (
-                    <View>
-                        <TextInput
-                            placeholder={item.imperialTypeValue}
-                        />
-                    </View>
-                )}
-            />
+        <View style={converterScreenStyles.valuesBlockContainer}>
+            <View style={converterScreenStyles.valuesBlockBackground}>
+                <Text style={converterScreenStyles.valuesTitle}>{item.type}</Text>
+                <FlatList
+                    data={item.values}
+                    nestedScrollEnabled={true}
+                    renderItem={({ item }) => (
+                        <View style={converterScreenStyles.valuesSectionsContainer}>
+                            <View style={converterScreenStyles.valuesItemContainer}>
+                                <TextInput
+                                    style={converterScreenStyles.valuesItem}
+                                    placeholder={'1'}
+                                />
+                                <View
+                                    style={converterScreenStyles.valuesimperialTitleItemContainer}>
+                                    <Text>{item.imperialTypeValue}</Text>
+                                </View>
+                            </View>
+                            <SwitchValueArrow />
+                            <View style={converterScreenStyles.valuesItemContainer}>
+                                <TextInput
+                                    style={converterScreenStyles.valuesItem}
+                                    placeholder={item.value.toString()}
+                                />
+                                <View
+                                    style={converterScreenStyles.valuesMetricTitleItemContainer}>
+                                    <Text>{item.metricTypeValue}</Text>
+                                </View>
+                            </View>
+                        </View>
+                    )}
+                />
+            </View>
         </View>
     )
     return (
@@ -37,6 +60,7 @@ export default function Convertor() {
                 <FlatList
                     data={valuesListToView}
                     renderItem={renderItem}
+                    style={converterScreenStyles.valuesListContainer}
                     showsVerticalScrollIndicator={false}
                     showsHorizontalScrollIndicator={false}
                 />
--- styles/converterScreenStyles.ts
+++ styles/converterScreenStyles.ts
@@ -11,4 +11,42 @@ export const converterScreenStyles = StyleSheet.create({
 		width: '100%',
 		height: '100%',
 	},
+	valuesListContainer: {},
+	valuesTitle: {
+		textAlign: 'center',
+	},
+	valuesSectionsContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	valuesBlockBackground: {
+		backgroundColor: '#7fff00',
+		borderRadius: 20,
+		padding: 20,
+	},
+	valuesBlockContainer: {
+		padding: 20,
+	},
+	valuesItemContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		width: '40%',
+		backgroundColor: '#66cdaa',
+		borderWidth: 1,
+		alignItems: 'center',
+	},
+	valuesItem: {
+		backgroundColor: '#66cdaa',
+		// borderWidth: 1,
+	},
+	valuesMetricTitleItemContainer: {
+		justifyContent: 'flex-start',
+		width: '30%',
+	},
+	valuesimperialTitleItemContainer: {
+		justifyContent: 'flex-start',
+		width: '40%',
+	},
 });
