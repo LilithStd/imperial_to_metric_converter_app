2f4245e6bc382903ddce9632c30e5492adcb606a
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2025-06-07 13:25:35 +0300

    start refactoring converter component

--- app/converter.tsx
+++ app/converter.tsx
@@ -38,20 +38,14 @@ export default function Convertor() {
 
     const valuesGroups = [{ type: VALUES_TYPES.ALL, values: [{ id: 'all', imperialTypeValue: '', metricTypeValue: '', value: 0 }] }, { type: VALUES_TYPES.FAVORITES, values: [{ id: 'favorites', imperialTypeValue: '', metricTypeValue: '', value: 0 }] }, ...valuesListToView];
 
-    // const handleImperialChange = (text: string, value: number) => {
-    //     setValueImperial(text);
-    //     const num = parseFloat(text);
-    //     if (!isNaN(num)) {
-    //         setValueMetric((convertImperialToMetric(GLOBAL_VALUES_TYPES.IMPERIAL, num, value)).toFixed(2));
-    //     }
-    //     // else {
-    //     //     setValueMetric('');
-    //     // }
-    // };
-
-    const handleImperialChange = (text: string) => {
-        setImperialDraft(text);
+    const handleImperialChange = (text: string, value: number) => {
+        setValueImperial(text);
+        const num = parseFloat(text);
+        if (!isNaN(num)) {
+            setValueMetric((convertImperialToMetric(GLOBAL_VALUES_TYPES.IMPERIAL, num, value)).toFixed(2));
+        }
     };
+
 
     const handleMetricChange = (text: string, value: number) => {
         setValueMetric(text);
@@ -59,14 +53,9 @@ export default function Convertor() {
         if (!isNaN(num)) {
             setValueImperial((convertImperialToMetric(GLOBAL_VALUES_TYPES.METRIC, num, value)).toFixed(2));
         }
-        //  else {
-        //     setValueImperial('');
-        // }
     };
 
-    // const handleEndEditing = () => {
-    //     // setValueImperial('')
-    // }
+
     const getImperialValue = (id: string) => {
         if (isExistsValueIsArray(resultAfterConvertion, id)) {
             const found = resultAfterConvertion.find((el) => el.id === id);
@@ -81,18 +70,8 @@ export default function Convertor() {
 
     const handleFocus = (id: string) => {
         setActiveInput(id);
-        const exists = resultAfterConvertion.find(item => item.id === id);
-        setImperialDraft(exists?.imperialValues ?? '');
+
     };
-
-    const resultImperialView = () => {
-        const isExistsValues = isExistsValueIsArray(resultAfterConvertion, activeInput)
-        return isExistsValues ? resultAfterConvertion.find((element) => element.id === activeInput)?.imperialValues : valueImperial
-    }
-
-    const resultMetricView = () => {
-
-    }
 
     const renderGroupItem = ({ item }: { item: RESULT_VALUES_TYPE }) => (
         <TouchableOpacity style={converterScreenStyles.valuesGroupItem}
@@ -103,22 +82,6 @@ export default function Convertor() {
 
     )
 
-    const handleEndEditing = () => {
-        if (!activeInput) return;
-
-        setResultAfterConvertion(prev => {
-            const exists = prev.find(item => item.id === activeInput);
-            if (exists) {
-                return prev.map(item =>
-                    item.id === activeInput
-                        ? { ...item, imperialValues: imperialDraft }
-                        : item
-                );
-            } else {
-                return [...prev, { id: activeInput, imperialValues: imperialDraft, metricValues: '' }];
-            }
-        });
-    };
 
     useEffect(() => {
         if (isExistsValueIsArray(resultAfterConvertion, activeInput)) {
@@ -150,10 +113,9 @@ export default function Convertor() {
                                             style={converterScreenStyles.valuesItem}
                                             placeholder={'1'}
                                             keyboardType='numeric'
-                                            value={imperialDraft}
+                                            value={getImperialValue(item.id)}
                                             onFocus={() => handleFocus(item.id)}
-                                            onChangeText={handleImperialChange}
-                                            onEndEditing={handleEndEditing}
+                                            onChangeText={(text) => handleImperialChange(text, item.value)}
                                         />
                                         <View
                                             style={converterScreenStyles.valuesImperialTitleItemContainer}>
